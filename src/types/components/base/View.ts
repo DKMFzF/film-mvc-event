d57df492184699c3415661b74a8d6283559d91b0
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-05-02 13:38:58 +0700

    refactor(types): add commets

--- src/types/components/base/View.ts
+++ src/types/components/base/View.ts
@@ -1,15 +1,12 @@
+// глобальная вьюшка
 export interface IView<T, S = object> {
-	// отображение для заданного типа данных
-	element: HTMLElement; // корневой элемент
-	copy(settings?: S): IView<T>; // копирующий конструктор
-	render(data?: Partial<T>): HTMLElement; // метод рендера
+	element: HTMLElement;
+	copy(settings?: S): IView<T>;
+	render(data?: Partial<T>): HTMLElement;
 }
 
 export interface IViewConstructor<T, S> {
-	// конструктор отображения
-	// получает на вход клонированный шаблон
-	// или существующий элемент,
-	// а также настройки для отображения
+	// получает на вход клонированный шаблон или существующий элемент,
 	new (root: HTMLElement, settings: S): IView<T>;
 }
 
